c507d985cfdf69e91c485fd5aeaecf68dd169edd
noone2097 <rafaelmiano1120@gmail.com>
2025-05-08 23:12:59 +0800

    add logo

--- app/Providers/Filament/LoonPanelProvider.php
+++ app/Providers/Filament/LoonPanelProvider.php
@@ -40,6 +40,7 @@ class LoonPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandLogo(fn () => view('filament.components.logo'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/filament/components/logo.blade.php
@@ -0,0 +1,13 @@
+<div class="flex items-center gap-2">
+    <img
+        src="{{ asset('images/pos.png') }}"
+        alt="POS Logo"
+        style="height: 3rem;"
+        width="48"
+        height="48"
+        fetchpriority="high" />
+    <div class="flex flex-col">
+        <div class="text-lg font-bold">POS Loon</div>
+        <div class="text-xs text-gray-500 dark:text-gray-400">Loon, Bohol</div>
+    </div>
+</div>
